9d555d8f66fb67be6451027cba1c58d1355cec6d
Hafiz <hafiz.pariabi@gmail.com>
2017-03-09 13:15:34 +0700

    Use view model in view

--- l6reader/ViewController.swift
+++ l6reader/ViewController.swift
@@ -10,21 +10,37 @@ import UIKit
 import RxSwift
 import RxDataSources
 
+
+class DisplayArticleMock : DisplayArticle {
+    
+    func display() -> Observable<[Article]> {
+        let articles = [Article(title:"mine"), Article(title:"yours"), Article(title:"the truth")]
+        return Observable.just(articles)
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
    
     private let data = Observable<[String]>.just(["first element", "second element", "third element"])
     
+    
+    private var articleListingVM = ArticleListingViewModel(displayArticle: DisplayArticleMock())
+    
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        data.bindTo(tableView.rx.items(cellIdentifier: "articleItemCell")) {
+        
+        articleListingVM.articles.asDriver()
+            .drive(tableView.rx.items(cellIdentifier: "articleItemCell")) {
                 _, article, cell in
-                cell.textLabel?.text = article
+                cell.textLabel?.text = article.title
             }.disposed(by: disposeBag)
+        
+        articleListingVM.didLoad()
+        
     }
 }
 
