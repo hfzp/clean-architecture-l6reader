621412c5d5fa21a8e18612bd4c2aa3e638ec0f9e
Hafiz <hafiz.pariabi@gmail.com>
2017-03-09 13:01:45 +0700

    Init rxdatasources

--- l6reader/ViewController.swift
+++ l6reader/ViewController.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
+import RxSwift
+import RxDataSources
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+   
+    private let data = Observable<[String]>.just(["first element", "second element", "third element"])
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        data.bindTo(tableView.rx.items(cellIdentifier: "articleItemCell")) {
+                _, article, cell in
+                cell.textLabel?.text = article
+            }.disposed(by: disposeBag)
     }
 }
 
